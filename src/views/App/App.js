93f00e5776a5c67afb64b57ca9948446c1fa285e
Mikulew <mikulew@gmail.com>
2019-08-09 19:43:39 +0200

    Configure files' architecture

---
+++ src/views/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from 'components/Button/Button';
+
+const App = () => (
+  <div>
+    <h1>Hello React.js developer!</h1>
+    <Button>Hekki hekki</Button>
+  </div>
+);
+
+export default App;
